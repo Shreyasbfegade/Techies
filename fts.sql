00215181041d1b9549548596374b1ee96166d8ee
itsdhruvrpandey01 <130044341+itsdhruvrpandey01@users.noreply.github.com>
2024-02-24 17:00:52 +0530

    Updated fts.sql
    Added New Tables 
    Venue
    Time 
    Booked

--- fts.sql
+++ fts.sql
@@ -1,30 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Dec 22, 2018 at 06:07 PM
--- Server version: 5.7.19-log
--- PHP Version: 7.2.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fts_5`
---
 CREATE DATABASE IF NOT EXISTS `fts_5` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `fts_5`;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `files`
@@ -43,13 +23,6 @@ CREATE TABLE IF NOT EXISTS `files` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `files`
---
-
-
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `movements`
@@ -70,17 +43,6 @@ CREATE TABLE IF NOT EXISTS `movements` (
 ALTER TABLE files
 ADD attachment_type VARCHAR(255) AFTER attachment;
 
-
---
-
-
--- Dumping data for table `movements`
---
-
-
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `notifications`
 --
@@ -94,14 +56,6 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `notifications`
---
-
-
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -119,73 +73,59 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 
-
-
--- Create the 'users' table
--- CREATE TABLE IF NOT EXISTS `users` (
---   `id` int(11) NOT NULL AUTO_INCREMENT,
---   `name` varchar(200) NOT NULL,
---   `email` varchar(200) NOT NULL,
---   `password` varchar(200) NOT NULL,
---   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
---   `usertype` int(11), -- Add a foreign key reference to 'usertype'
---   PRIMARY KEY (`id`),
---   FOREIGN KEY (`usertype`) REFERENCES `usertype`(`id`) -- Define the foreign key relationship
--- );
-
-
 ALTER TABLE movements
 ADD count VARCHAR(255) AFTER updated_at;
 --
 -- Dumping data for table `users`
 --
 
+CREATE TABLE usertype (
+  id INT PRIMARY KEY,
+  usertype VARCHAR(255)
+);
 
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- INSERT INTO `usertype`(`id`, `usertype`) VALUES ('1','Principal');
--- INSERT INTO `usertype`(`id`, `usertype`) VALUES ('2','Staff');
--- INSERT INTO `usertype`(`id`, `usertype`) VALUES ('3','Admin');
--- INSERT INTO `usertype`(`id`, `usertype`) VALUES ('4','Student');
+INSERT INTO usertype(id, usertype) VALUES ('1','Principal');
+INSERT INTO usertype(id, usertype) VALUES ('2','Staff');
+INSERT INTO usertype(id, usertype) VALUES ('3','Admin');
+INSERT INTO usertype(id, usertype) VALUES ('4','Student');
 
 INSERT INTO users (id, name, email, password, is_admin, usertype) VALUES (, 'admin', 'admin@gmail.com','admin' ,'1', '3');
 
-
-CREATE TABLE Date (
-    date DATE PRIMARY KEY
-);
-
 CREATE TABLE Venue (
-    venue_name VARCHAR(100),
-    date DATE,
-    FOREIGN KEY (date) REFERENCES Date(date)
+    venueCode int PRIMARY KEY,
+    venue_name VARCHAR(100)
 );
+
+INSERT INTO `venue` (`venueCode`, `venue_name`) VALUES ('1', 'Auditorium');
+INSERT INTO `venue` (`venueCode`, `venue_name`) VALUES ('2', 'Turf');
+INSERT INTO `venue` (`venueCode`, `venue_name`) VALUES ('3', 'College Ground');
+
+
 CREATE TABLE Time (
     index_column INT PRIMARY KEY,
     time_slot VARCHAR(100)
 );
-ALTER TABLE Venue
-ADD COLUMN index INT AUTO_INCREMENT PRIMARY KEY;
+
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('1', '9.00-10.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('2', '10.00-11.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('3', '11.00-12.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('4', '12.00-13.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('5', '13.00-14.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('6', '14.00-15.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('7', '15.00-16.00');
+INSERT INTO `time` (`index_column`, `time_slot`) VALUES ('8', '16.00-17.00');
+
 
 CREATE TABLE Booked (
     index_column INT PRIMARY KEY,
     date DATE,
     venue_id INT,
-    time_index INT
+    time_index INT,
+    FOREIGN KEY (venue_id) REFERENCES Venue(venueCode),
+    FOREIGN KEY (time_index) REFERENCES Time(index_column)
 );
 
-
-ALTER TABLE `booked` ADD FOREIGN KEY (`date`) REFERENCES `date`(`date`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `booked` ADD FOREIGN KEY (`time_index`) REFERENCES `time`(`index_column`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE `booked` ADD FOREIGN KEY (`venue_id`) REFERENCES `venue`(`index`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-INSERT INTO time (index_column, time_slot) VALUES ('1', '9.00-10.00');
-INSERT INTO time (index_column, time_slot) VALUES ('2', '10.00-11.00');
-INSERT INTO time (index_column, time_slot) VALUES ('3', '11.00-12.00');
-INSERT INTO time (index_column, time_slot) VALUES ('4', '12.00-13.00');
-INSERT INTO time (index_column, time_slot) VALUES ('5', '13.00-14.00');
-INSERT INTO time (index_column, time_slot) VALUES ('6', '14.00-15.00');
-INSERT INTO time (index_column, time_slot) VALUES ('7', '15.00-16.00');
-INSERT INTO time (index_column, time_slot) VALUES ('8', '16.00-17.00');
+-- Inserting some random bookings for the date '24-02-24'
+INSERT INTO Booked (index_column, date, venue_id, time_index) VALUES (1, '2024-02-24', 1, 1); -- Booking at 9.00-10.00 in Auditorium
+INSERT INTO Booked (index_column, date, venue_id, time_index) VALUES (2, '2024-02-24', 2, 2); -- Booking at 10.00-11.00 in Turf
+INSERT INTO Booked (index_column, date, venue_id, time_index) VALUES (3, '2024-02-24', 1, 5); -- Booking at 13.00-14.00 in Auditorium
