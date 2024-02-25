c9a890a5371e647fbaee74b2d9df0884571e8f02
sahil-2411 <126896734+sahil-2411@users.noreply.github.com>
2024-02-25 09:21:44 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,14 +15,60 @@ if (isset($_SESSION['user'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style></style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap');
+     body {
+            padding-top: 20px; 
+        }
+
+        body {
+            background-image: url('bg.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #333;
+        }
+
+        .heading {
+            margin-top: 10px;
+            text-align: center;
+            font-family: 'Montserrat', sans-serif;
+            
+        
+        }
+
+        .heading-container {
+          background-color: #f2f2f2; 
+            padding: 20px;
+        }
+
+        .form-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        @media (max-width: 768px) {
+            .form-container {
+                flex-direction: column;
+            }
+        }
+    </style>
     <title>Login</title>
 </head>
 <body>
+<div class="container-fluid">
+<div class="heading-container">
+            <h1 class="heading">College Event Handling</h1>
+        </div>
     <section class="vh-100">
         <div class="container py-5 h-100">
           <div class="row d-flex align-items-center justify-content-center h-100">
             <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <div class="d-flex justify-content-center">
+                            <h1 class="heading">Sign in</h1>
+                            <!-- <img src="bg-icon.jpg" class="img-fluid mb-4" alt="Icon image" style="height: 100px; width: 150px;"> -->
+                        </div>
               <form action="logging.php" method = "POST">
                 <!-- Email input -->
                 <div class="form-outline mb-4">
@@ -45,6 +91,9 @@ if (isset($_SESSION['user'])) {
                     <option value="2">Staff</option>
                     <option value="4">Student</option>
                     <option value="5">HOD</option>
+                    <option value="6">Cultural Commitee</option>
+                    <option value="7">Technical Commitee</option>
+                    <option value="8">Sports Commitee</option>
                     
                   </select>
                   <!-- <input type="number" name ="type" id="form1Example33" placeholder="Type of user" class="form-control form-control-lg" /> -->
@@ -62,10 +111,12 @@ if (isset($_SESSION['user'])) {
                 </form>
               </div>
               <div class="col-md-8 col-lg-7 col-xl-6">
-                <img src="fts5.jpeg"
+                <img src="fts1.jpg"
                   class="img-fluid" alt="Phone image">
               </div>
           </div>
         </div>
       </section>
+      </div>
+
 </body>
